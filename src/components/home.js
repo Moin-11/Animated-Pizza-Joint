2f213f44dafb05574cb734075814c06f31fec86a
Moin-11 <Moin11.dev@gmail.com>
2021-01-14 14:57:18 +0530

    made home scale animation scale differently

--- src/components/home.js
+++ src/components/home.js
@@ -28,7 +28,7 @@ const buttonvariants = {
     },
   },
   hover: {
-    scale: 1.2,
+    scale: 1.1,
     textShadow: "0px 0px 8px rgb(255,255,255)",
     boxShadow: "0px 0px 8px rgb(255,255,255)",
     transition: {
